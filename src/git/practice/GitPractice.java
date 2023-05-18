8fa3643ef27e99d3ca20cd1935ac1a6ea9a79998
Rajpreet <kau12862@sheridancollege.ca>
2023-05-18 15:15:25 -0400

    Practice of Git

---
+++ src/git/practice/GitPractice.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package git.practice;
+
+/**
+ *
+ * @author Rajpreet Kaur,2023
+ */
+public class GitPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("My name is Rajpreet");
+    }
+
+}
